28db64ed6d86e2a2e7006f005cb451d5ea4bf92a
MichaelC <m@michaelcullum.com>
2015-08-22 17:44:38 +0100

    Add notes about other php versions

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -14,12 +14,14 @@ rm -rf /var/www/phpbb/phpBB/config.php
 cd /vagrant
 php /var/www/phpbb/phpBB/install_new/phpbbcli.php install ./phpbb-install-config.yml
 
-# Uncomment this to recompile php7 on provisioning (NOTE: Slow)
-# makephp 7
+# Uncomment this to recompile php7 on provisioning (NOTE: Slow). You can also change it
+# to compile other versions of php if you wish (e.g 5.6)
+# /vagrant/makephp 7
 
 # Set the virtual host up
 sudo rm -rf /etc/nginx/conf.d/default.conf
 sudo cp /vagrant/default.conf /etc/nginx/conf.d/default.conf
 
-# Set to PHP 7 debug mode and restart php-fpm
-newphp 7 debug
+# Set to PHP 7 debug mode and restart php-fpm.
+# Adapt this line if you want to set a different deafult php version
+/vagrant/newphp 7 debug
